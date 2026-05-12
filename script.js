37ee0e22c844053fac5468275a3f376433cc6923
edenchisvo <106526310+edenchisvo@users.noreply.github.com>
2026-05-12 23:13:27 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', () => {
-    const header = document.querySelector('header');
-
-    if(window.scrollY > 50){
-        header.style.background = 'rgba(0,0,0,0.85)';
-    } else {
-        header.style.background = 'rgba(0,0,0,0.5)';
-    }
-});
